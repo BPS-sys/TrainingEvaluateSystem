1c02da89c91663c51169193579b0f9d95a1e3e68
BPS-sys <prgrmsilver@gmail.com>
2024-10-22 13:25:44 +0900

    スコアをリスト形式に変更

--- ConfidenceCheck/Confidence.py
+++ ConfidenceCheck/Confidence.py
@@ -5,27 +5,29 @@ import pykakasi
 
 class confidence:
 
-    def __init__(self):
+    def __init__(self, score_list):
         self.kakasi = pykakasi.kakasi()
         self.kakasi.setMode('K', 'H')
         self.kakasi.setMode('J', 'H')
         self.conversion = self.kakasi.getConverter()
-        self.score = 0
         self.student_infos = None
         self.infos = None
         # self.last_deal_time = datetime.datetime.now().strftime('%H:%M:%S')
         self.last_deal_time = '2024-10-18 15:34:12'
         self.last_deal_time = datetime.datetime.strptime(self.last_deal_time, '%Y-%m-%d %H:%M:%S')
         self.out_list = ['おもいます']
+        self.score_list = score_list
 
+    # 情報取得
     def send(self, infos):
         self.infos = infos
 
+    # 自信チェック
     def check(self):
         dealed_flag = False
         temp_time = self.last_deal_time
         self.student_infos = self.infos[1:]
-        for student_info in self.student_infos:  # 受講生個人のデータ
+        for student_index, student_info in enumerate(self.student_infos):  # 受講生個人のデータ
             for time_stump, text in student_info:
                 student_time_stump = datetime.datetime.strptime(time_stump, '%Y-%m-%d %H:%M:%S')
                 # 受講生の発言した時間が過去に処理した最も遅い時間の発言よりも後なら
@@ -36,15 +38,13 @@ class confidence:
                     hira_text = self.conversion.do(text)
                     for out_text in self.out_list:
                         if out_text in hira_text:
-                            self.score -= 100
+                            self.score_list[student_index] -= 100
                             dealed_flag = True
                         else:
-                            self.score += 100
+                            self.score_list[student_index] += 100
                             dealed_flag = True
         # 処理した最も遅い時間を更新
         self.last_deal_time = temp_time
-        print(self.score)
-        print(self.last_deal_time)
         return dealed_flag
 
 if __name__ == '__main__':
