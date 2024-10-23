9b4af8fc0d9fadba38d935eaff0828c3617f2735
BPS-sys <prgrmsilver@gmail.com>
2024-10-23 14:17:44 +0900

    no message

--- PostureCheck/JudgePosture.py
+++ PostureCheck/JudgePosture.py
@@ -195,37 +195,18 @@ def taking():
                             # ランドマークに対応するラベルを表示
                             # cv2.putText(image, label, (cx + 10, cy + 10),
                             #             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+                        if bottom_is_good(results.pose_landmarks.landmark, image_width):
+                          cv2.putText(image, 'good', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+                        else:
+                          cv2.putText(image, 'bad', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
 
-                    # 座っているかを判定
-                    if is_sitting(results.pose_landmarks.landmark, image_height):
-                        cv2.putText(image, 'Sitting', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
-                        # 左膝と左足首のX座標を使って９０度になっているかを判定
-                        knee = posture_scoring_knee_ankle(results.pose_landmarks.landmark, image_height, image_width)
-                        knee_score_list.append(posture_scoring_knee_ankle(results.pose_landmarks.landmark, image_width))
-                        cv2.putText(image, f'Posture (Knee-Ankle): {knee}', (30, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
-
-                        # 姿勢が良いかを判定
-                        posture = posture_scoring(results.pose_landmarks.landmark, image_height, image_width)
-                        posture_score_list.append(posture_scoring(results.pose_landmarks.landmark, image_width))
-                        cv2.putText(image, f'Posture: {posture}', (30, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
-
-                        #首の判定
                         neck = neck_scoring(results.pose_landmarks.landmark, image_height, image_width)
-                        neck_score_list.append(neck_scoring(results.pose_landmarks.landmark, image_height, image_width))
                         cv2.putText(image, f"neck:{neck}", (30, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-
-                        #手首の判定
-                        hand = hand_scoring(results.pose_landmarks.landmark, image_height, image_width)
-                        hand_score_list.append(hand_scoring(results.pose_landmarks.landmark, image_height, image_width))
-                        cv2.putText(image, f"hand:{hand}", (30, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2, cv2.LINE_AA)
-                    else:
-                        cv2.putText(image, 'Standing', (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
-
                 # 画像を表示
                 cv2.imshow('Selected Landmarks', image)
 
                 if cv2.waitKey(5) & 0xFF == 27:
                     break
 
-  cap.release()
 taking()
+cap.release()
